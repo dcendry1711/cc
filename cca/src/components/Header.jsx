75bc0d079c995ac417e6057c18bea03a652ab110
dcendry1711 <daniel.cendry@gmail.com>
2025-08-19 14:02:57 +0200

    Header component (static)

---
+++ cca/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header>
+            <img className="header-icon" src="/cci.png" alt="chef claude app icon"/>
+            <h1 className="header-title">Chef Claude</h1>
+        </header>
+    )
+}
