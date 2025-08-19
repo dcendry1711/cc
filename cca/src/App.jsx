9c8219d2fc15b21a147c41dfda0a6e52af45631c
dcendry1711 <daniel.cendry@gmail.com>
2025-08-19 14:39:55 +0200

    form section (style)

--- cca/src/App.jsx
+++ cca/src/App.jsx
@@ -4,6 +4,10 @@ function App() {
   return (
     <main>
       <Header />
+        <form className="ingredients-form">
+          <input className="ingredients-input" type="text" placeholder="e.g. oregano"/>
+          <button className="add-ingredient-btn" type="submit">+ Add ingredient</button>
+        </form>
     </main>
   )
 }
