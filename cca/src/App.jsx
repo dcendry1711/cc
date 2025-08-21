c8160e7cf7f16222fe7a2a66adccc2d7c97f690c
dcendry1711 <daniel.cendry@gmail.com>
2025-08-21 13:03:11 +0200

    update of project review

--- cca/src/App.jsx
+++ cca/src/App.jsx
@@ -40,7 +40,7 @@ function App() {
             </ul>
           </section>
         }
-        
+
     </main>
   )
 }
@@ -51,7 +51,8 @@ export default App
   Przegląd projektu:
   1. Użytkownik po uruchomieniu aplikacji widzi nagłówek oraz formularz umożliwiający wprowadzanie składników (ZROBIONE)
   2. W momencie wprowadzania kolejnych składników zmienia się layout strony tak aby wyświetlać listę z kolejnymi dodanymi składnikami w formie <ul></ul> (ZROBIONE)
-  3. Dla wprowadzonych składników umożliwiam call to action w postaci przycisku generującego przepis
-  4. Naciśnięcie przycisku wysyła listę składników do modelu AI, który generuje dla nas przepis
-  5. Przepis wyświetlamy pod sekcją z przyciskiem generującym przepis
+  3. lista składników jest wyświetlana dynamicznie. Póki nie ma składników w tablicy, nie wyświetla się sekcja z listą składników.
+  4. Dla wprowadzonych składników (przynajmniej 3) umożliwiam call to action w postaci przycisku generującego przepis
+  5. Naciśnięcie przycisku wysyła listę składników do modelu AI, który generuje dla nas przepis
+  6. Przepis wyświetlamy pod sekcją z przyciskiem generującym przepis
 */
