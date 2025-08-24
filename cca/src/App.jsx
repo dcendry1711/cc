df25157fc4993e6261c9c9bf85a659c2656e2756
dcendry1711 <daniel.cendry@gmail.com>
2025-08-24 20:23:04 +0200

    implement ai to my code, handle response, display recipe, apply remove btn for generated recipe

--- cca/src/App.jsx
+++ cca/src/App.jsx
@@ -4,6 +4,7 @@ import Recipe from './components/Recipe'
 import IngredientsList from './components/IngredientsList' 
 import Cta from './components/Cta'
 import Form from './components/Form'
+import {getRecipeFromChefClaude} from './ai.js'
 
 function App() {
 
@@ -24,28 +25,36 @@ function App() {
 
   //funckja obsługująca status zmiennej isRecipeShown
 
-  function generateRecipe(){
+  async function generateRecipe(){
     setIsRecipeShown( prevRecipeShown => !prevRecipeShown)
+    const readyRecipe = await getRecipeFromChefClaude(ingredientsArr)
+    setGeneratedRecpie( prevRecipe => readyRecipe)
   }
+
+  //funkcja obsługująca czyszczenie wygenerowanego przepisu
 
   function clearRecipe(){
     setIngredientsArr( prevIngredientsArr => [])
     setIsRecipeShown( prevIsRecipeShown => false)
+    setGeneratedRecpie( prevRecipe => '')
   }
+
+  //deklaracja state przetrzymującego informację o wygenerowanym przepisie przez AI (domyślnie brak wpisu dla zmiennej)
+
+  const [generatedRecipe, setGeneratedRecpie] = useState('')
 
   return (
     <main>
 
       <Header />
 
-        <Form addIngredient={addIngredient}/>
+      <Form addIngredient={addIngredient}/>
 
-        {ingredientsArr.length > 0 && <IngredientsList  ingredientsArr={ingredientsArr}/>}
+      {ingredientsArr.length > 0 && <IngredientsList  ingredientsArr={ingredientsArr}/>}
         
+      {ingredientsArr.length > 2 && <Cta generateRecipe={generateRecipe} isRecipeShown={isRecipeShown}/>}
 
-        {ingredientsArr.length > 2 && <Cta generateRecipe={generateRecipe} isRecipeShown={isRecipeShown}/>}
-
-        {isRecipeShown && <Recipe clearRecipe={clearRecipe} />}
+      {isRecipeShown && <Recipe generatedRecipe={generatedRecipe}  clearRecipe={clearRecipe} />}
 
     </main>
   )
@@ -62,6 +71,8 @@ export default App
   5. Dynamiczne wyświetlanie informacji o wymaganej ilości składników w celu wyświetlenia sekcji cta (ZROBIONE)
   6. Dla wprowadzonych składników (przynajmniej 3) umożliwiam call to action w postaci przycisku generującego przepis (ZROBIONE)
   7a. Naciśnięcie przycisku na CTA wyświetli na sztywno przygotowany przepis -> wersja testowa (ZROBIONE)
-  7b. Naciśnięcie przycisku wysyła listę składników do modelu AI, który generuje dla nas przepis
-  8. Przepis wyświetlamy pod sekcją z przyciskiem generującym przepis
+  7b. Naciśnięcie przycisku wysyła listę składników do modelu AI, który generuje dla nas przepis (ZROBIONE)
+  8. Przepis wyświetlamy pod sekcją z przyciskiem generującym przepis (ZROBIONE)
+  9. Przepis zwrócony przez AI należy odpowiednio wystylować
+  10. Stworzyć przycisk usuwający wygenerowany przycisk tak aby wrócić do stanu wyjściowego umożliwiającego wprowadzanie nowych składników
 */
--- cca/src/components/Cta.jsx
+++ cca/src/components/Cta.jsx
@@ -5,7 +5,7 @@ export default function Cta(props){
             <h3>Ready for a recipe?</h3>
             <p>Generate a recipe from your list of ingredients.</p>
             </div>
-            <button onClick={props.generateRecipe} className="cta-btn" disabled={props.isRecipeShown}>Get a recipe</button>
+            <button onClick={props.generateRecipe} className="cta-btn">Get a recipe</button>
         </section>
     )
 }
